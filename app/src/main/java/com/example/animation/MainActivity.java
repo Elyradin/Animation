a90d30701dfaa2d57cd9aa299faffa1f15b44cef
Elyradin <elyra.dina.oktaviani@gmail.com>
2023-05-17 14:10:04 +0700

    im

---
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ObjectAnimator mAnimator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imgView = findViewById(R.id.img);
+        Button btn = findViewById(R.id.btn_start);
+
+        mAnimator = ObjectAnimator.ofFloat(imgView, "y", 1200);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAnimator.setDuration(1000);
+                mAnimator.start();
+            }
+        });
+    }
+}
